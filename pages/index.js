a31c835c4a20bc7985db162c25da878ffbbc1247
Johan <57840332+johannasendi@users.noreply.github.com>
2021-11-24 20:58:33 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <>
+      <div>
+        <div className="container mx-auto">
+          <div className="flex">
+            <div className="w-3/12 uppercase text-2x1 font-semibold tracking-widest" />
+              Johan 
+          </div>
+        </div>
+      </div>
+    </>
+   
+  )
+}
